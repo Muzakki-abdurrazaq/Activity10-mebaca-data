f468ca0084c9fb18121c79b266a90594b8286df7
Muzakki Abdurrazaq <muzakkiabdurrazaq123@gmail.com>
2021-03-09 20:37:14 +0700

    Activity OnClick

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.sampleconstraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText edemail, edpassword;
+    String nama, password,massage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin=findViewById(R.id.button);
+        edemail=findViewById(R.id.editText);
+        edpassword=findViewById(R.id.editText2);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nama = edemail.getText().toString();
+                password = edpassword.getText().toString();
+                if (nama.equals("admin@mail.com")&& password.equals("123"){
+                    Toast t=Toast.makeText(getApplicationContext(),
+                            "Login sukses");
+                    t.show();
+                }
+                else{
+                    Toast t=Toast.makeText(getApplicationContext(),
+                            "Login gagal");
+                    t.show();
+                }
+
+
+
+            }
+        }
     }
 }
